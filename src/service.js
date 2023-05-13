1784a15bb22b12d0f21b1e6ff3ca244ac847f600
Rikoshet1 <riki97563@gmail.com>
2023-05-13 22:20:49 +0300

    first commit

---
+++ src/service.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+//const apiUrl = "http://localhost:5017"
+const apiClient = axios.create({
+  baseURL: "http://localhost:5017",
+});
+
+apiClient.interceptors.response.use(
+  response => response,
+  error => {
+    console.error(error);
+    return Promise.reject(error);
+  }
+);
+export default {
+  getTasks: async () => {
+    const result = await apiClient.get('/items')    
+    return result.data;
+  },
+
+  addTask: async(name)=>{
+    console.log('addTask', name)
+    //TODO
+    await apiClient.post(`/items`,{name:name,isComplete:false})
+    return {};
+  },
+
+  setCompleted: async(id, isComplete)=>{
+    console.log('setCompleted', {id, isComplete})
+    //TODO
+    await apiClient.put(`/items/${id}`,isComplete)
+    return {};
+  },
+
+  deleteTask:async(id)=>{
+    console.log('deleteTask')
+    await apiClient.delete(`/items/${id}`)
+  }
+};
